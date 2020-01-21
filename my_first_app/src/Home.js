7e058157eb6ce5bdc57ddce54bd20075a875295e
Shalitha <shalithax@gmail.com>
2020-01-21 18:55:08 +0530

    Add home class

---
+++ my_first_app/src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomeClass extends Component{
+    render(){
+        return(
+            <div className="home">
+                <h3>this is home</h3>
+            </div>
+        )
+    }
+}
+
+export default HomeClass
