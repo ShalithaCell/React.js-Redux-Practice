1eb0840b2cbf2cd947df4cbb123d26507c183d80
Shalitha <shalithax@gmail.com>
2020-01-22 10:56:05 +0530

    optimized

--- my_first_app/src/Home.js
+++ my_first_app/src/Home.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 
 const HomeClass = ({obj}) => {
-        const objList = obj.map(o => {
-            return (
-                <div className="home" key={o.id}>
-                    <h3>this is content </h3>
-                    <h4>ID : {o.id} </h4>
-                    <h4>Name : {o.name} </h4>
-                    <h4>DOB : {o.dob} </h4>
-                    
-                </div>
-            )
-        })
-
+        
         return (
             <div className="obj-list">
-                {objList}
+                {
+                    obj.map(o => {
+                        return (
+                            <div className="home" key={o.id}>
+                                <h3>this is content </h3>
+                                <h4>ID : {o.id} </h4>
+                                <h4>Name : {o.name} </h4>
+                                <h4>DOB : {o.dob} </h4>
+                                
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
 }
