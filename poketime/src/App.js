e5e95b045530df52d2a6698dfea91ddf053f1568
Shalitha <shalithax@gmail.com>
2020-01-26 11:57:29 +0530

    set routing

---
+++ poketime/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Navbar from './components/navbar';
+import {BrowserRouter, Route} from 'react-router-dom'
+import Home from './components/home'
+import About from './components/about'
+import Contact from './components/contact'
+
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar/> 
+          <Route path='/home' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+        </div> 
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ poketime/src/components/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>In laborum et amet quis. Voluptate ullamco exercitation ipsum cillum enim proident. Occaecat et magna tempor eiusmod elit do. Commodo ex esse ea incididunt nisi aute reprehenderit elit consequat elit.
+
+Elit minim proident consequat incididunt quis do ex anim aliquip aute sunt. Pariatur aliquip dolor laborum in laboris aute ullamco mollit ut labore enim duis. Laborum pariatur excepteur labore laborum ea occaecat laboris esse Lorem pariatur laboris in. Qui laborum aliquip enim adipisicing id aute.
+
+Nostrud id sunt irure ad laboris culpa eiusmod eu esse veniam adipisicing fugiat nostrud deserunt. Culpa occaecat sunt aliqua elit magna veniam. Ullamco minim anim fugiat ea adipisicing culpa. Do dolor elit cupidatat cillum dolore. Sunt adipisicing laboris aute sit Lorem quis nostrud pariatur cupidatat exercitation ut. Esse cillum reprehenderit anim elit occaecat do consequat do ex deserunt ea. Esse proident aliqua veniam mollit non.
+
+Excepteur labore pariatur ut anim pariatur aute qui proident elit irure. Excepteur occaecat et anim ut. Aliqua adipisicing ex sit ut qui eiusmod proident voluptate. Do veniam tempor ad velit enim aute elit cupidatat. Deserunt ut sit anim ullamco deserunt dolore fugiat duis pariatur aute elit. Tempor consequat non eiusmod commodo et velit eu ut in officia laborum cupidatat mollit minim. Anim fugiat do reprehenderit aute mollit sunt veniam excepteur magna.</p>
+        </div>
+    )
+}
+
+export default Home
